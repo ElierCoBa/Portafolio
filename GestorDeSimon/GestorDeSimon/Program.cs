9606a5766cd9ca710bf55f6815e7b975b9823bc0
ElierCoBa <eliercortes12@gmail.com>
2021-02-25 14:35:58 -0600

    Proyecto de gestor de simón dice

---
+++ GestorDeSimon/GestorDeSimon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorDeSimon
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            List<int> ListaDePuntuaciones = new List<int>();
+            int Indice = 0;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormularioDeSimon(ListaDePuntuaciones, Indice));
+        }
+    }
+}
